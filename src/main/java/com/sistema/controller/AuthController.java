d48ad643239faec519997f5d883e56d3c07baa6e
Luanderson Gomes <gomesluanderson@gmail.com>
2025-12-04 09:09:12 -0300

    ajustes login

--- src/main/java/com/sistema/controller/AuthController.java
+++ src/main/java/com/sistema/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class AuthController {
     
